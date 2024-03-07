503451916d1ae0ad7bc010b947472a348079ded6
MateuszGajdowicz <155658500+MateuszGajdowicz@users.noreply.github.com>
2024-03-07 19:18:01 +0100

    usepage

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-function pobierzDane() {
-    let h1 = document.getElementById("h1");
-            // Pobieramy parametry z adresu URL
-            var urlParams = new URLSearchParams(window.location.search);
-
-            // Sprawdzamy, czy istnieje parametr "username" w adresie URL
-            if(urlParams.has('username')) {
-                // Pobieramy wartość parametru "username" z adresu URL
-                var username = urlParams.get('username');
-
-                // Wyświetlamy komunikat powitalny
-                h1.textContent = "hello" + username;
-            }
-        }
-
-        // Wywołujemy funkcję pobierania danych po załadowaniu strony
-        window.onload = pobierzDane;
+//function pobierzDane() {
+//    let h1 = document.getElementById("h1");
+//            // Pobieramy parametry z adresu URL
+//            var urlParams = new URLSearchParams(window.location.search);
+//
+//            // Sprawdzamy, czy istnieje parametr "username" w adresie URL
+//            if(urlParams.has('username')) {
+//                // Pobieramy wartość parametru "username" z adresu URL
+//                var username = urlParams.get('username');
+//
+//                // Wyświetlamy komunikat powitalny
+//                h1.textContent = "hello" + username;
+//            }
+//        }
+//
+//        // Wywołujemy funkcję pobierania danych po załadowaniu strony
+//        window.onload = pobierzDane;
