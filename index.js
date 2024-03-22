26e3e2f06c65de99db5d904a8f1673f1b6d017e2
MateuszGajdowicz <155658500+MateuszGajdowicz@users.noreply.github.com>
2024-03-22 12:33:17 +0100

    usepage

---
+++ index.js
@@ -0,0 +1,22 @@
+//username = document.getElementById("h1");
+//document.addEventListener('DOMContentLoaded', function() {
+//    function getQueryVariable(variable) {
+//        var query = window.location.search.substring(1);
+//        var vars = query.split('&');
+//        for (var i = 0; i < vars.length; i++) {
+//            var pair = vars[i].split('=');
+//            if (pair[0] === variable) {
+//                return decodeURIComponent(pair[1]);
+//            }
+//        }
+//        return null;
+//    }
+//
+//    var username = getQueryVariable('username');
+//    if (username) {
+//            h1.textContent('Hello, ' + username + '!');
+//        } else {
+//            h1.textContent = ('Hello, guest!');
+//        }
+//
+//    }
